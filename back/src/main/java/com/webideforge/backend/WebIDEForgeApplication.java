87ab9c152daf93ed80369e142de59ec1b2e14f0f
gunooh <gunooh12@gmail.com>
2025-08-10 03:42:46 +0900

    feat: backend > springboot 세팅

---
+++ back/src/main/java/com/webideforge/backend/WebIDEForgeApplication.java
@@ -0,0 +1,12 @@
+package com.webideforge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIDEForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIDEForgeApplication.class, args);
+	}
+}
